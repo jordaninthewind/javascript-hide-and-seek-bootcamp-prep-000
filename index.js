0bc87325539145871253dff424f1e18a4cdea628
jordaninthewind <jordan.kline@gmail.com>
2017-10-11 23:43:23 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,22 @@
+function getFirstSelector(selector) {
+  var select = document.querySelector(selector);
+  return select;
+}
+
+function nestedTarget() {
+  var nest = document.querySelector('div#nested div.target');
+  return nest;
+}
+
+function increaseRankBy(n) {
+  var index = document.querySelectorAll('ul.ranked-list li');
+    for (let i = 0; i < index.length; i++) {
+      var int = parseInt(index[i], 10);
+      index[i].innerHTML = int + n;
+      return int + n;
+    }
+}
+
+function deepestChild(){
+
+}
