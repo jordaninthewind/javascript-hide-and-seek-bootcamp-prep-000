ee331af41fc5ac4ae923c6420e0ba7068a76b17a
jordaninthewind <jordan.kline@gmail.com>
2017-10-12 04:06:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,5 +19,5 @@ function increaseRankBy(n) {
 
 function deepestChild(){
   const last = document.getElementById('div#grand-node').querySelectorAll('div');
-  return last[inc].textContent;
+  return last.textContent;
 }
