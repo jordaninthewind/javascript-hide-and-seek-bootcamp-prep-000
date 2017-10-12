f62f756da63e1d089aa4cb1ba53f9053e5a5a325
jordaninthewind <jordan.kline@gmail.com>
2017-10-12 04:24:40 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+function getFirstSelector(selector) {
+  var select = document.querySelector(selector);
+  return select;
+}
+
+function nestedTarget() {
+  var nest = document.querySelector('div#nested div.target');
+  return nest;
+}
+
+function increaseRankBy(n) {
+  const index = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+    for (let i = 0; i < index.length; i++) {
+      var int = parseInt(index[i].textContent);
+      index[i].innerHTML = int + n;
+    }
+}
+
+function deepestChild(){
+  const last = document.getElementById('grand-node').querySelectorAll('div');
+  var length = last.length;
+  return last[length - 1];
+  // for (let i = 0; i < last.length; i++) {
+    // pull out most deeply nested child from div#grand-node
+
+    // iterate over elements & call querySelector and querySelectorAll
+}
