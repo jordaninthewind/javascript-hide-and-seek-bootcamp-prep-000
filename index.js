3d55baafb7bcc61f0cc7a14ac3f8053d259d0c58
jordaninthewind <jordan.kline@gmail.com>
2017-10-12 00:12:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function increaseRankBy(n) {
   //     index[i].innerHTML = int + n;
   //   }
   const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li')
-   
+
   for (let i = 0; i < lis.length; i++) {
     lis[i].innerHTML = (i + n).toString()
   }
